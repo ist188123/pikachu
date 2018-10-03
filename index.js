f490121f86f85cf79724d6483cc054027e02043d
ist188123 <paulorosario@sapo.pt>
2018-10-04 00:25:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -654,25 +654,40 @@ client.on("message", async (msg) => {
 
       })
       
-     msg.guild.channels.find("name", "quest").sendMessage("teste");
+    
       
-     const publicaQuest = new Discord.RichEmbed()
-        .setTitle(quest)
-        .setAuthor(pokestop, "https://exraidspinhalnovo.webnode.pt/_files/200000044-1157e1263e/450/pstop.png")
-        /*
-         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-         */
-        .setColor(0x00AE86)
-        .setDescription(missao)
-        .setFooter("PN PoGo Raids, pubicado ", "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
+     const embed = new Discord.RichEmbed()
+  .setTitle("This is your title, it can hold 256 characters")
+  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x00AE86)
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("This is a field title, it can hold 256 characters",
+    "This is a field value, it can hold 2048 characters.")
+  /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+  .addField("Inline Field", "They can also be inline.", true)
+  /*
+   * Blank field, useful to create some space.
+   */
+  .addBlankField(true)
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 
+  
 
-        .setThumbnail(questimagem)
-
-        .setTimestamp();
-
-      msg.guild.channels.find("name", "quest").sendMessage({ publicaQuest });
-      // msg.guild.channels.find("name", "quest").sendMessage(quest+" "+missao+" "+pokestop+" "+questimagem);
+      msg.guild.channels.find("name", "quest").sendMessage({ embed });
+  
      
 
       
