f3c6390bd4a4641d16fa2e7aa0619bb06e90a7d4
ist188123 <paulorosario@sapo.pt>
2019-06-18 02:49:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -374,182 +374,165 @@ function leinforaid(pCode, cb) {  //leraud
 
 
   //lista todos os elementos que tem a regra
-  function criaRaid(canalRaid, bicho, adicional, participantesRaid, sponser) {
+  async function criaRaid(canalRaid, bicho, adicional, participantesRaid, sponser) {
 
     try {
+        var titulo = "RAID " + canalRaid.substr(5);
+        var tiporaid = canalRaid.substring(5, 6);
+        let bosscp = "";
+        var status = "Desconhecido";
+        var cpiv = "Desconhecido"
 
+        var ovo = "";
+        var braid = ovo;
+        //var bicho="";
 
+        cor = "0x00AE86";
 
+        //-------
 
-
-      let bosscp = "";
-      var status = "Desconhecido";
-      var cpiv = "Desconhecido"
-
-      var ovo = "";
-      var braid = ovo;
-      //var bicho="";
-
-      cor = "0x00AE86";
-
-      //-------
-      
-       //-------
-     var titulo = "RAID " + canalRaid.substr(5);
-var tiporaid = canalRaid.substring(5, 6);
-//verifica se o numero é positivo ou negarivo
-var thoras = titulo.split("-");
-       var horas = thoras[thoras.length - 3];
-var minutos_abrir_aberta = thoras[thoras.length - 2];
-if(minutos_abrir_aberta.startsWith("a")){
-    minutos_abrir_aberta=minutos_abrir_aberta.replace("a","");
-}else{
-    minutos_abrir_aberta=minutos_abrir_aberta.replace("f","-");
-}
-
-//--------------------------
-
-var horas_relogio=thoras[thoras.length - 1];
-horas_relogio=horas_relogio.replace("h",":")
-
-//tira o hora que foi marcada a raid do nome do canal
-var horas_relogio_horas_minutos=horas_relogio.split(":")
-var today = new Date();
-var date= new Date(today.getFullYear(), today.getMonth(), today.getDate(),horas_relogio_horas_minutos[0], horas_relogio_horas_minutos[1]);
-var dformat =  [mzero(date.getHours()),
-    mzero(date.getMinutes())].join(':');
-
-//-----------------------------
-
-//cria o titulo da raid
-var titulo_raid=""
-for (var i = 0; i < thoras.length-3; i++) {
-    
-    titulo_raid = titulo_raid + " " + thoras[i]
-    
-  
-  }
- 
-//console.log("Ntitulo "+titulo_raid)
-
-//-----------
-
-y = thoras;
-
-var local=titulo_raid;
-//console.log ("aberta_fechada: "+minutos_abrir_aberta);
-
-var choca=minutos_abrir_aberta;
-if(parseInt(choca)<0){
-    choca=-(45+parseInt(choca))
-}	    
- //console.log("hora atual: "+add_minutes(new Date(),0));
-//mais esta ouvo +30
-// menos esta aberta -30
- //horas que abre o ovo
-       var abre=add_minutes(date, choca).toString();
-      // console.log("Abre ovo : "+abre);
-      //-----------------------
-   var desaparece=  parseInt(choca)+45;
-
- // console.log("tempo da raid "+ desaparece)
-      
-var hinicio=add_minutes(date, choca).toString();
-      var hovo=add_minutes(date, choca).toString();
-
-var hfim=add_minutes(date, desaparece).toString()
-      
-  //console.log("Termina : "+add_minutes(date, desaparece).toString());
-       
-      
-      
-      
-      
-      
-      //-------
-      
-    
-      
-      
-      
-      
-      
-     
-      
-      //----- novo 
-  
-
-      var raids = jsonData.map(x => x)
-      var tamanhoFicheiro = Object.keys(raids).length;
-
-
-
-      switch (tiporaid) {
-
-        case "3":
-          ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000027-959cf96a39/200/4.png";
-          braid = ovo;
-          break;
-        case "4":
-          ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000027-959cf96a39/200/4.png";
-          braid = ovo;
-          break;
-        case "5":
-          ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000019-4d5f84e5ec/200/Egg_Raid_Legendary.png";
-          braid = ovo;
-          break;
-      }
-
-
-
-      for (var x = 0; x < tamanhoFicheiro; x++) {
-
-        if (tiporaid == raids[x].nivel && bicho == raids[x].boss) {
-
-          braid = raids[x].imagem;
-
-          cpiv = raids[x].cpiv + " " + raids[x].bosted;
-          bosscp = raids[x].bosscp + " " + raids[x].bosstipo;
-          status = raids[x].fraco + "\n" + raids[x].counter;
+        //-------
+        var titulo = "RAID " + canalRaid.substr(5);
+        var tiporaid = canalRaid.substring(5, 6);
+        //verifica se o numero é positivo ou negarivo
+        var thoras = titulo.split("-");
+        var horas = thoras[thoras.length - 3];
+        var minutos_abrir_aberta = thoras[thoras.length - 2];
+        if (minutos_abrir_aberta.startsWith("a")) {
+            minutos_abrir_aberta = minutos_abrir_aberta.replace("a", "");
+        } else {
+            minutos_abrir_aberta = minutos_abrir_aberta.replace("f", "-");
         }
-      }
+
+        //--------------------------
+
+        var horas_relogio = thoras[thoras.length - 1];
+        horas_relogio = horas_relogio.replace("h", ":")
+
+        //tira o hora que foi marcada a raid do nome do canal
+        var horas_relogio_horas_minutos = horas_relogio.split(":")
+        var today = new Date();
+        var date = new Date(today.getFullYear(), today.getMonth(), today.getDate(), horas_relogio_horas_minutos[0], horas_relogio_horas_minutos[1]);
+        var dformat = [mzero(date.getHours()),
+        mzero(date.getMinutes())].join(':');
+
+        //-----------------------------
+
+        //cria o titulo da raid
+        var titulo_raid = ""
+        for (var i = 0; i < thoras.length - 3; i++) {
+
+            titulo_raid = titulo_raid + " " + thoras[i]
+
+
+        }
+
+        //console.log("Ntitulo "+titulo_raid)
+
+        //-----------
+
+        y = thoras;
+
+        var local = titulo_raid;
+        //console.log ("aberta_fechada: "+minutos_abrir_aberta);
+
+        var choca = minutos_abrir_aberta;
+        if (parseInt(choca) < 0) {
+            choca = -(45 + parseInt(choca))
+        }
+        //console.log("hora atual: "+add_minutes(new Date(),0));
+        //mais esta ouvo +30
+        // menos esta aberta -30
+        //horas que abre o ovo
+        var abre = add_minutes(date, choca).toString();
+        // console.log("Abre ovo : "+abre);
+        //-----------------------
+        var desaparece = parseInt(choca) + 45;
+
+        // console.log("tempo da raid "+ desaparece)
+
+        var hinicio = add_minutes(date, choca).toString();
+        var hovo = add_minutes(date, choca).toString();
+
+        var hfim = add_minutes(date, desaparece).toString()
+
+        switch (tiporaid) {
+            case "1":
+                ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000097-88ffa8a090/200/nivel1.png";
+                break;
+            case "2":
+                ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000097-88ffa8a090/200/nivel1.png";
+                break;
+
+            case "3":
+                ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000027-959cf96a39/200/4.png";
+
+                break;
+            case "4":
+                ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000027-959cf96a39/200/4.png";
+
+                break;
+            case "5":
+                ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000019-4d5f84e5ec/200/Egg_Raid_Legendary.png";
+
+                break;
+
+        }
+
+
+    
+            //--- fim novo          
 
 
 
-      //--- fim novo          
+        var result = await leinforaid('http://pnraidspn.atwebpages.com/raid.php', async function (pCLatLng) {
+            pCLatLng.forEach(nivel => {
+               
+
+                if (nivel.boss == bicho && nivel.nivel==tiporaid ) {
+                    
+                    braid = nivel.imagem
+                    cpiv = nivel.cpiv + " " + nivel.bosted;
+                    bosscp = nivel.bosscp + " " + nivel.bosstipo;
+                    status = nivel.fraco + "\n" + nivel.counter;
+                }
+
+
+
+            })
+       
 
 
 
 
+            if (sponser == "exraid") {
+                cor = "0XFEAFEA";
+                ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000023-29ab72ab0f/450/Mewtwo.png"
+                horas = horas + "\nGinásio em pontuação para o Deoxys";
+            }
+            //----  FIM TIPO RAID ---
 
 
-      if (sponser == "exraid") {
-        cor = "0XFEAFEA";
-        ovo = "https://exraidspinhalnovo.webnode.pt/_files/200000023-29ab72ab0f/450/Mewtwo.png"
-        horas = horas + "\nGinásio em pontuação para o Deoxys";
-      }
-      //----  FIM TIPO RAID ---
-
-
-      disparaRaid(local, horas, canalRaid, adicional, participantesRaid, ovo, braid, cpiv, status, adicional, bosscp,hinicio,hfim,hovo);
-      //  msg.guild.channels.find("name", "raids-pinhal-novo").sendMessage(msg.guild.channels.find("name", msg.channel.name)+"\n"+membersWithRole.join("\n"));
+           await disparaRaid(local, horas, canalRaid, adicional, participantesRaid, ovo, braid, cpiv, status, adicional, bosscp, hinicio, hfim, hovo);
+          
+        });//leinforaid
 
     } catch (err) {
-      console.log(err);
+        console.log(err);
 
 
     }
 
 
 
-  }
+}
 
 
 
 
 
 
-  function disparaRaid(local, horas, canal, treinadores, total, ovo, bicho, cpiv, status, adicional, cpboss,hinicio,hfim,hovo) {
+  async function  disparaRaid(local, horas, canal, treinadores, total, ovo, bicho, cpiv, status, adicional, cpboss,hinicio,hfim,hovo) {
 //+"\nAbre: "+hinicio+" Termina :"+hfim +" "+hovo
     //msg.reply("disparaRaid");
     const embed = new Discord.RichEmbed()
